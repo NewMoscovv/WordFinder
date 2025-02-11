5895a64fe0419e5f07695f787a5c1a832dd94ae2
Кирсанов.Laptop <kir.commersion@internet.ru>
2025-02-11 23:53:39 +0300

    add method GetSheetList to getting list of sheets in excel file

--- internal/excel/excel.go
+++ internal/excel/excel.go
@@ -1,11 +1,13 @@
 package excel
 
 import (
+	lg "Excel_word_checker/pkg/logger"
+	"fmt"
 	"github.com/xuri/excelize/v2"
 )
 
 type Excel struct {
-	fileName *excelize.File
+	file *excelize.File
 }
 
 func New(fileName string) (*Excel, error) {
@@ -16,6 +18,25 @@ func New(fileName string) (*Excel, error) {
 	}
 
 	return &Excel{file}, nil
+}
+
+func (e *Excel) GetSheetList(logger lg.Logger) {
+	var sheetString string
+
+	sheetList := e.file.GetSheetList()
+
+	if len(sheetList) == 1 {
+		sheetString = sheetList[0]
+	} else {
+		for _, sheet := range sheetList {
+			sheetString = fmt.Sprintf("%s ", sheet)
+		}
+	}
+
+	logger.Info.Printf("Обнаружено %d страниц: %s. Выберите какой лист необходимо просканировать.\n"+
+		"Для этого необходимо написать название листа или \"ВСЕ\" для сканирования всех листов",
+		len(sheetList), sheetString)
+
 }
 
 func (e *Excel) FindWord(word string) (string, error) {
