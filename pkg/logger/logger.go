fe2e6bfaee7a5174885ee88cc8cf3411dc423c9e
Кирсанов.Laptop <kir.commersion@internet.ru>
2025-02-11 23:15:37 +0300

    add logger

---
+++ pkg/logger/logger.go
@@ -0,0 +1,21 @@
+package logger
+
+import (
+	"log"
+	"os"
+)
+
+type Logger struct {
+	Info *log.Logger
+	Err  *log.Logger
+}
+
+func Init() *Logger {
+
+	logger := Logger{}
+
+	logger.Info = log.New(os.Stdout, "WordF1nder: ", int(" "[0]))
+	logger.Err = log.New(os.Stdout, "[!ОШИБКА!]\nWordF1nder: ", int(" "[0]))
+
+	return &logger
+}
