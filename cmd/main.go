4366e4f61976fc7bd425ff28c13f965d49e3b5a6
Кирсанов.Laptop <kir.commersion@internet.ru>
2025-02-12 00:15:53 +0300

    add variator.go to work with dif types of files

--- cmd/main.go
+++ cmd/main.go
@@ -1,4 +1,23 @@
 package main
 
+import (
+	lg "Excel_word_checker/pkg/logger"
+	"fmt"
+)
+
 func main() {
+
+	logger := lg.Init()
+	logger.Info.Print("Добро пожаловать! Я помогу найти Вам слова в различных файлах.\n\n" +
+		"Сейчас поддерживаются файлы: Excel")
+	logger.Info.Print("Выберете, какие файлы желаете отсканировать." +
+		"Для этого введите названия, что написаны выше☝️\n")
+
+	var finderType string
+
+	_, err := fmt.Scanln(&finderType)
+	if err != nil {
+		logger.Err.Print(err.Error())
+	}
+
 }
---
+++ internal/variator/variator.go
@@ -0,0 +1,15 @@
+package variator
+
+import (
+	"strings"
+)
+
+func SetFileType(fileType string) {
+	fileType = strings.ToLower(fileType)
+
+	switch fileType {
+	case "excel":
+		// here is not logic.
+		// -- pls implement me!
+	}
+}
